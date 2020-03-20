90f147ee2eff5fe8a856cdf4f4737d1f164b515f
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-20 16:52:47 -0600

    Add default status to pets table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200320032933) do
+ActiveRecord::Schema.define(version: 20200320224300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20200320032933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
-    t.integer "status"
+    t.integer "status", default: 0
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
