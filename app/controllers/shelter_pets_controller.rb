03b1a66915c75da80b9f1e62fae89e2dbd9b5867
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-21 11:57:13 -0600

    Move new and create methods to shelter-pets controller

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -4,4 +4,21 @@ class ShelterPetsController < ApplicationController
     @shelter = Shelter.find(params[:shelter_id])
   end
 
+  def new
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    shelter = Shelter.find(params[:shelter_id])
+    shelter.pets.create(pet_params)
+
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
+
+  private
+    
+  def pet_params
+    params.permit(:image, :name, :description, :approx_age, :sex)
+  end
+
 end
