b08a3ddadffc66b204375fcdc7e44edf71f011a2
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-21 23:28:51 -0600

    Test and function to make all pet names as links

---
+++ spec/features/pets/pets_links_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "When I click a pet name anywhere on site", type: :feature do
+  it "I go to that pet show page" do
+    shelter1 = Shelter.create!(name: "Burt's Barn",
+                                 address: "123 Sesame Street",
+                                 city: "New York",
+                                 state: "NY",
+                                 zip: "12345")
+      
+    pet1 = shelter1.pets.create!(image: "https://assets.change.org/photos/3/yk/di/kLYkdIaPKknZpoD-800x450-noPad.jpg?1519383791",
+                                 name: "Penelope",
+                                 approx_age: 1,
+                                 sex: "female")
+    
+    visit "/pets"
+
+    click_on "#{pet1.name}"
+    expect(current_path).to eq("/pets/#{pet1.id}")
+    expect(page).to have_content(pet1.description)
+
+    visit "/shelters/#{shelter1.id}/pets"
+    click_on "#{pet1.name}"
+    expect(current_path).to eq("/pets/#{pet1.id}")
+    expect(page).to have_content(pet1.description)
+  end
+end
