1f866fbee886b2c57b595f46d46d296dcff505ba
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-21 18:37:29 -0600

    Add test for delete shelter link

--- spec/features/shelters/shelter_index_spec.rb
+++ spec/features/shelters/shelter_index_spec.rb
@@ -54,5 +54,43 @@ RSpec.describe "As a visitor", type: :feature do
       expect(current_path).to eq("/shelters/#{shelter2.id}/edit")
       expect(page).to have_field(:name)
     end
+   
+    it "I can click link next to each shelter to delete it" do
+     shelter1 = Shelter.create!(name: "Burt's Barn",
+                                 address: "123 Sesame Street",
+                                 city: "New York",
+                                 state: "NY",
+                                 zip: "12345")
+      shelter2 = Shelter.create!(name: "Ernie's Everhome",
+                                 address: "505 Elmwood Drive",
+                                 city: "Chelsea",
+                                 state: "MA",
+                                 zip: "02468")
+
+      visit "/shelters" 
+      
+      expect(page).to have_content(shelter1.name)
+      expect(page).to have_content(shelter2.name)
+
+      within "#shelter-#{shelter1.id}" do
+        click_link "Delete"
+      end
+      
+      expect(page).not_to have_content(shelter1.name)
+      expect(page).to have_content(shelter2.name)
+  
+      within "#shelter-#{shelter2.id}" do
+        click_link "Delete"
+      end
+      
+      expect(page).not_to have_content(shelter1.name)
+      expect(page).not_to have_content(shelter2.name)      
+    end
   end
 end
+# As a visitor
+# When I visit the shelter index page
+# Next to every shelter, I see a link to delete that shelter
+# When I click the link
+# I am returned to the Shelter Index Page
+#   where I no longer see that shelter
