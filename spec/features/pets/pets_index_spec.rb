69b9fb604a5a1b5ffd1d6d6f73cb09658501deba
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-21 20:05:26 -0600

    Add test and link in pets index to edit pet

--- spec/features/pets/pets_index_spec.rb
+++ spec/features/pets/pets_index_spec.rb
@@ -53,5 +53,42 @@ RSpec.describe "As a visitor", type: :feature do
         expect(page).to have_content(shelter2.name)
       end
     end
+
+    it "I can click link next to each pet to edit it" do
+      shelter1 = Shelter.create!(name: "Burt's Barn",
+                                 address: "123 Sesame Street",
+                                 city: "New York",
+                                 state: "NY",
+                                 zip: "12345")
+      
+      pet1 = shelter1.pets.create!(image: "https://assets.change.org/photos/3/yk/di/kLYkdIaPKknZpoD-800x450-noPad.jpg?1519383791",
+                                   name: "Penelope",
+                                   approx_age: 1,
+                                   sex: "female")
+      pet2 = shelter1.pets.create!(image: "https://external-preview.redd.it/P4X2K5llPbMkRkmFkycSPdDGAp051xJl0UF50bZRId4.jpg?auto=webp&s=5231b2f36d623b3258d36adbfbaf9be305f0d419",
+                                   name: "Petey",
+                                   approx_age: 1,
+                                   sex: "male")
+    
+      visit "/pets"
+
+      within "#pet-#{pet1.id}" do
+        click_link "Update Pet"
+      end
+      
+      expect(current_path).to eq("/pets/#{pet1.id}/edit")
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:approx_age)
+      
+      visit "/pets"
+
+      within "#pet-#{pet2.id}" do
+        click_link "Update Pet"
+      end
+      
+      expect(current_path).to eq("/pets/#{pet2.id}/edit")
+      expect(page).to have_field(:name)
+      expect(page).to have_field(:approx_age)
+    end
   end
 end
